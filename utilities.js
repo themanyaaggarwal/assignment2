066820c1129f8e4a717a1beee41dcfc0a2caf494
themanyaaggarwal <144986897+themanyaaggarwal@users.noreply.github.com>
2023-11-29 12:01:19 +0530

    Add files via upload

---
+++ utilities.js
@@ -0,0 +1,102 @@
+// RAJASTHAN
+function jaipuron(){
+    document.getElementById('map_info_rajasthan').src='graphics/jaipur.png' ;
+}
+function jaipuroff(){
+    document.getElementById('map_info_rajasthan').src='graphics/transparent_rajasthan.png' ;
+}
+
+function jodhpuron(){
+    document.getElementById('map_info_rajasthan').src='graphics/jodhpur.png' ;
+}
+function jodhpuroff(){
+    document.getElementById('map_info_rajasthan').src='graphics/transparent_rajasthan.png' ;
+}
+
+function udaipuron(){
+    document.getElementById('map_info_rajasthan').src='graphics/udaipur.png' ;
+}
+function udaipuroff(){
+    document.getElementById('map_info_rajasthan').src='graphics/transparent_rajasthan.png' ;
+}
+
+function jaisalmeron(){
+    document.getElementById('map_info_rajasthan').src='graphics/jaisalmer.png' ;
+}
+function jaisalmeroff(){
+    document.getElementById('map_info_rajasthan').src='graphics/transparent_rajasthan.png' ;
+}
+
+function ranthamboreon(){
+    document.getElementById('map_info_rajasthan').src='graphics/ranthambore.png' ;
+}
+function ranthamboreoff(){
+    document.getElementById('map_info_rajasthan').src='graphics/transparent_rajasthan.png' ;
+}
+
+function mountabuon(){
+    document.getElementById('map_info_rajasthan').src='graphics/mountabu.png' ;
+}
+function mountabuoff(){
+    document.getElementById('map_info_rajasthan').src='graphics/transparent_rajasthan.png' ;
+}
+
+
+// HIMACHAL
+function shimlaon(){
+    document.getElementById('map_info_himachal').src='graphics/shimla.png' ;
+}
+function shimlaoff(){
+    document.getElementById('map_info_himachal').src='graphics/transparent_himachal.png' ;
+}
+
+function manalion(){
+    document.getElementById('map_info_himachal').src='graphics/manali.png' ;
+}
+function manalioff(){
+    document.getElementById('map_info_himachal').src='graphics/transparent_himachal.png' ;
+}
+
+function kasaulion(){
+    document.getElementById('map_info_himachal').src='graphics/kasauli.png' ;
+}
+function kasaulioff(){
+    document.getElementById('map_info_himachal').src='graphics/transparent_himachal.png' ;
+}
+
+function dalhousieon(){
+    document.getElementById('map_info_himachal').src='graphics/dalhousie.png' ;
+}
+function dalhousieoff(){
+    document.getElementById('map_info_himachal').src='graphics/transparent_himachal.png' ;
+}
+
+
+// GOA
+function anjunaon(){
+    document.getElementById('map_info_goa').src='graphics/anjuna.png' ;
+}
+function anjunaoff(){
+    document.getElementById('map_info_goa').src='graphics/transparent_goa.png' ;
+}
+
+function aguadaon(){
+    document.getElementById('map_info_goa').src='graphics/aguada.png' ;
+}
+function aguadaoff(){
+    document.getElementById('map_info_goa').src='graphics/transparent_goa.png' ;
+}
+
+function churchon(){
+    document.getElementById('map_info_goa').src='graphics/church.png' ;
+}
+function churchoff(){
+    document.getElementById('map_info_goa').src='graphics/transparent_goa.png' ;
+}
+
+function carnivalon(){
+    document.getElementById('map_info_goa').src='graphics/carnival.png' ;
+}
+function carnivaloff(){
+    document.getElementById('map_info_goa').src='graphics/transparent_goa.png' ;
+}
